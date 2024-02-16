d3b61c7dbfed0e0c13104c7c05ac8e7b79340beb
Tingting Wei <twei3@academic.rrc.ca>
2024-02-16 13:01:26 -0600

    Update the routes.tb file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
+
 Rails.application.routes.draw do
+
   root "articles#index"
 
   resources :articles
-end
-
-Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,11 +12,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
 
-Rails.application.routes.draw do
   get "/articles", to: "articles#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
+
 
